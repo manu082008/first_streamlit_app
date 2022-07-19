7d4fc5ea23b60a1603490ef824a11fde8eca62a5
manu082008 <98627386+manu082008@users.noreply.github.com>
2022-07-19 14:50:42 +0530

    Update streamlit_app.py
    Added emopjis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
-streamlit.title('Healthy Eating')
-streamlit.text('A healthy eating plan: Emphasizes vegetables, fruits, whole grains, and fat-free or low-fat dairy products.')
-streamlit.text('Includes lean meats, poultry, fish, beans, eggs, and nuts.')
-streamlit.text('Limits saturated and trans fats, sodium, and added sugars.')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

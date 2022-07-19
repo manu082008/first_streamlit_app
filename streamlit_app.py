79f715309b37d896625173a7881cf5e9277c8669
manu082008 <98627386+manu082008@users.noreply.github.com>
2022-07-19 14:37:22 +0530

    Update streamlit_app.py
    Added additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,3 @@
 import streamlit
-streamlit.title('Hello')
+streamlit.title('Healthy Eating')
+streamlit.text('A healthy eating plan: Emphasizes vegetables, fruits, whole grains, and fat-free or low-fat dairy products. Includes lean meats, poultry, fish, beans, eggs, and nuts. Limits saturated and trans fats, sodium, and added sugars.')

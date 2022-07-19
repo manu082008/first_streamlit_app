1a4fc0acb39de9867c6e3802c83888fca678a221
manu082008 <98627386+manu082008@users.noreply.github.com>
2022-07-19 14:46:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('Healthy Eating')
-streamlit.text('A healthy eating plan: Emphasizes vegetables, fruits, whole grains, and fat-free or low-fat dairy products. Includes lean meats, poultry, fish, beans, eggs, and nuts. Limits saturated and trans fats, sodium, and added sugars.')
+streamlit.text('A healthy eating plan: Emphasizes vegetables, fruits, whole grains, and fat-free or low-fat dairy products.')
+streamlit.text('Includes lean meats, poultry, fish, beans, eggs, and nuts.')
+streamlit.text('Limits saturated and trans fats, sodium, and added sugars.')
